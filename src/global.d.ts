cf9315785e6bafe06ba2c5b3aa3bd51091f72f2f
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-22 22:03:18 +0530

    Create global.d.ts

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+
+declare module '@prisma/client';
+declare module 'graphql';
+declare module 'graphql-subscriptions';
+declare module 'graphql-tag';
+declare module '@graphql-tools/schema';
+declare module '@apollo/server';
+declare module 'express';
+declare module 'ws';
+declare module 'dotenv';
+declare module 'cors';
+declare module 'body-parser';
